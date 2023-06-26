038ca74dec32246ce3af07d2d400fd29848f6e27
Giles Papworth <990812+djsuperchief@users.noreply.github.com>
2023-06-26 17:05:30 +0100

    #1 added test project

--- src/clumber/Core/TestRunner.cs
+++ src/clumber/Core/TestRunner.cs
@@ -1,10 +1,14 @@
 using System.IO;
+using Microsoft.Playwright;
 
 namespace Clumber.Core;
 
 public class TestRunner
 {
     private readonly string _testPackLocation;
+    private IBrowser _chromeInstance;
+    private IBrowser _firefoxInstance;
+    private IBrowser _webkitInstance;
 
     public TestRunner(string testPackLocation)
     {
@@ -17,7 +21,12 @@ public class TestRunner
         if (!Directory.Exists(_testPackLocation)) throw new Exceptions.TestLoaderException(string.Format(Resources.TestLocationNotFound, _testPackLocation));
         foreach (var pack in Directory.EnumerateDirectories(_testPackLocation))
         {
-
+            
         }
+    }
+
+    private async Task RunPack(string packLocation)
+    {
+
     }
 }
---
+++ tests/Clumber.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Clumber.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
